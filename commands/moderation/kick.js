4e6344a47b948ff5758619f3a089c97040505e73
Snuffz <51891016+Snuffz@users.noreply.github.com>
2020-11-25 19:34:31 -0300

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -11,7 +11,7 @@ module.exports = {
             return message.reply('sorry, you do not have permission for that!')
         }
 
-        const member = message.guild.members.cache.get(args[0].replace(/[<@>]/g, '')), // filters the first argument by removing <@>
+        const member = message.guild.members.cache.get(args[0].replace(/[<!@>]/g, '')), // filters the first argument by removing <@>
         reason = args.slice(1).join(' ') || 'no reason specified';
         if(!member) return message.reply("i couldn't find this member!");
         if(!member.kickable) return message.reply("i can't kick this member!");
